66366e18893cbe19e7ff6821ed3276fed323fa98
normandavid67 <normandavid67@gmail.com>
2012-12-30 23:00:55 +0000

    Removed unused logging import

--- JiniCommands/src/org/jini/commands/compress/ZipDirectory.java
+++ JiniCommands/src/org/jini/commands/compress/ZipDirectory.java
@@ -22,7 +22,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Enumeration;
-import java.util.logging.Logger;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 import java.util.zip.ZipOutputStream;
