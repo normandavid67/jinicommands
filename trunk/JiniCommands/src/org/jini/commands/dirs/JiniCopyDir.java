05fa4c7ecc509bc8689eb22ebb25c8a3e8ff0eae
normandavid67@gmail.com <normandavid67@gmail.com@5466fe5b-fca1-a824-9abf-828e199be8e7>
2013-01-01 08:05:06 +0000

    method copyDirectory : if dstDir cannot be made error is caught

--- trunk/JiniCommands/src/org/jini/commands/dirs/JiniCopyDir.java
+++ trunk/JiniCommands/src/org/jini/commands/dirs/JiniCopyDir.java
@@ -1,29 +1,29 @@
 /*
-New BSD License
-Copyright (c) 2012, Norman David <normandavid67@gmail.com>
-All rights reserved.
+ New BSD License
+ Copyright (c) 2012, Norman David <normandavid67@gmail.com>
+ All rights reserved.
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-    * Neither the name of the <organization> nor the
-      names of its contributors may be used to endorse or promote products
-      derived from this software without specific prior written permission.
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of the <organization> nor the
+ names of its contributors may be used to endorse or promote products
+ derived from this software without specific prior written permission.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL Norman David BE LIABLE FOR ANY
-DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL Norman David BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  
   
  For a copy of the License type 'license'
@@ -36,7 +36,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.ArrayList;
 import java.util.Calendar;
 import jinicommands.JiniCmd;
 import org.apache.commons.cli.BasicParser;
@@ -58,7 +57,6 @@ public class JiniCopyDir extends JiniCmd {
 
     CommandLineParser jcParser = new BasicParser();
     Options jcOptions = new Options();
-    //ArrayList<File> jcList = new ArrayList<File>();
     private boolean done;
     private String fromLocation;
     private String toLocation = getWorkingDirectory() + File.separator;
@@ -192,12 +190,17 @@ public class JiniCopyDir extends JiniCmd {
     public void copyDirectory(File srcDir, File dstDir) throws IOException {
         if (srcDir.isDirectory()) {
             if (!dstDir.exists()) {
-                dstDir.mkdir();
+                if (dstDir.mkdir() == false) {
+                    this.setJcError(true);
+                    this.addErrorMessages("Error : Could not create Directory " + dstDir.getAbsolutePath());
+                }
             }
 
-            String[] children = srcDir.list();
-            for (int i = 0; i < children.length; i++) {
-                this.copyDirectory(new File(srcDir, children[i]), new File(dstDir, children[i]));
+            if (this.jcError == false) {
+                String[] children = srcDir.list();
+                for (int i = 0; i < children.length; i++) {
+                    this.copyDirectory(new File(srcDir, children[i]), new File(dstDir, children[i]));
+                }
             }
         } else {
             // This method is implemented in Copying a File
