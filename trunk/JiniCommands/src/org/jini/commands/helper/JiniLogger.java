c7371b1372e95fcc5f6afd00f3b9b2df3873c98a
normandavid67@gmail.com <normandavid67@gmail.com@5466fe5b-fca1-a824-9abf-828e199be8e7>
2013-01-14 16:13:54 +0000

    Save File in Folder 'JiniLogs' in temp dir

--- trunk/JiniCommands/src/org/jini/commands/helper/JiniLogger.java
+++ trunk/JiniCommands/src/org/jini/commands/helper/JiniLogger.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.util.Date;
 import java.util.logging.FileHandler;
 import java.util.logging.Formatter;
+import java.util.logging.Level;
 import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 
@@ -24,7 +25,7 @@ public class JiniLogger {
      * This is the maximum number of log files to be created. If this number is
      * reached, the last file will be truncated and a new one will be created.
      */
-    int numLogFiles = 50;
+    int numLogFiles = 5;
     /**
      * If a file is full (as defined by 'limit' value above), files will be
      * renamed and a new log file will be created.
@@ -35,7 +36,22 @@ public class JiniLogger {
      * files will be renamed as: JiniLogs.log.0 JiniLogs.log.1 JiniLogs.log.2
      * ..and so on.
      */
-    String fileName = this.getOsSpecificTempDirectory()+File.pathSeparator+"JiniLogs.log";
+    String fileName = this.getOsSpecificTempDirectory() + File.pathSeparator + "JiniLogs.log";
+    /**
+     * Defining a log level. Level.ALL logs all levels of messages.
+     */
+    static Level logLevel = Level.ALL;
+    
+    
+    
+
+    public JiniLogger() {
+    }
+    
+    
+    
+    
+    
 
     /**
      * Getter method for append variable
@@ -71,7 +87,17 @@ public class JiniLogger {
         this.fileName = fileName;
     }
 
+    String getJiniLogDir() {
+        return this.getOsSpecificTempDirectory() + File.separator + "JiniLogs";
+
+    }
+
     public Logger getFileLogger() {
+        if (this.checkJiniLogDir() == false) {
+            this.createJiniLogDir();
+        }
+
+
         Formatter formatter = this.getCustomFormatter();
 
         /**
@@ -86,13 +112,10 @@ public class JiniLogger {
          */
         Logger logger = Logger.getLogger("org.jini.commands.helper.JiniLogger");
         try {
-
- 
-
             /**
              * Creating a file handler based on the above parameters
              */
-            FileHandler handler = new FileHandler(this.getFileName(), this.getLimit(), numLogFiles, this.isAppend());
+            FileHandler handler = new FileHandler(this.getJiniLogDir() + File.separator + this.getFileName(), this.getLimit(), numLogFiles, this.isAppend());
             if (formatter != null) {
                 handler.setFormatter(formatter);
             }
@@ -128,5 +151,56 @@ public class JiniLogger {
      */
     private String getOsSpecificTempDirectory() {
         return System.getProperty("java.io.tmpdir");
+    }
+
+    public void writeLog(String msg) {
+
+        /**
+         * Create a file logger with a custom log formatter If you provide the
+         * second parameter as null, a default XML Formatter will be applied,
+         * and the generated log file will be an XML
+         */
+        Logger fileLogger = this.getFileLogger();
+        /**
+         * Set the Log level.
+         */
+        fileLogger.setLevel(logLevel);
+        //   fileLogger.log(new LogRecord(logLevel, "We are starting to log into the file"));
+        //   fileLogger.fine("This is a fine level of message");
+        //  fileLogger.finer("This is a finer level of message");
+        fileLogger.finest(msg);
+    }
+
+    private boolean checkJiniLogDir() {
+        String directoryName = this.getJiniLogDir();
+
+        if ((directoryName != null) && (directoryName.length() > 0)) {
+            File dirTest = new File(directoryName);
+            if (dirTest.isDirectory()) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    private void createJiniLogDir() {
+
+        File dirName = new File(this.getJiniLogDir());
+
+        if (!dirName.exists()) {
+            dirName.mkdir();
+            // System.out.println(dirName.getAbsolutePath());
+        }
+    }
+
+    public static void main(String args[]) {
+        JiniLogger jl = new JiniLogger();
+        jl.setFileName("MyNewFileName.log");
+        for (int y = 0; y < 100;  y++) {
+            jl.writeLog("TEst");
+        }
+
     }
 }
