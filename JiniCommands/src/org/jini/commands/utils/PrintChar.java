e7d3ae3ae57d61e02f79bd66ab162e3d7a04f779
normandavid67 <normandavid67@gmail.com>
2013-01-18 09:19:00 +0000

    Print 50 chars in 1 row

--- JiniCommands/src/org/jini/commands/utils/PrintChar.java
+++ JiniCommands/src/org/jini/commands/utils/PrintChar.java
@@ -1,29 +1,29 @@
 /*
-New BSD License
-Copyright (c) 2012, Norman David <normandavid67@gmail.com>
-All rights reserved.
+ New BSD License
+ Copyright (c) 2012, Norman David <normandavid67@gmail.com>
+ All rights reserved.
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-    * Neither the name of the <organization> nor the
-      names of its contributors may be used to endorse or promote products
-      derived from this software without specific prior written permission.
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of the <organization> nor the
+ names of its contributors may be used to endorse or promote products
+ derived from this software without specific prior written permission.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL Norman David BE LIABLE FOR ANY
-DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL Norman David BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  
   
  For a copy of the License type 'license'
@@ -31,8 +31,8 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 package org.jini.commands.utils;
 
 /**
- * Prints a char at intervals of 500 miliseconds 
- * 
+ * Prints a char at intervals of 500 miliseconds
+ *
  * @author Norman David <normandavid67@gmail.com>
  * @since JiniCommands version 0.1
  * @version 1.0
@@ -44,35 +44,42 @@ public class PrintChar extends Thread {
 
     /**
      * flag to stop printing the char
-     * 
-     * @param stopPrinting 
+     *
+     * @param stopPrinting
      */
     public void setStopPrinting(boolean stopPrinting) {
         this.stopPrinting = stopPrinting;
     }
 
-    
     /**
      * setter method for the char to be printed
-     * 
-     * @param outPutChar 
+     *
+     * @param outPutChar
      */
-    
     public void setOutPutChar(String outPutChar) {
         this.outPutChar = outPutChar;
     }
-    
+
     /**
      * Start printing chars at 500 milisecond intervals
      */
-
     @Override
     public void run() {
         try {
-            for (;;) {
+            int counter = 0;
 
+            for (;;) {
                 if (this.stopPrinting == false) {
-                    System.out.print(this.outPutChar);
+
+                    if (counter < 51) {
+                        System.out.print(this.outPutChar);
+                         counter++;
+                    } else {
+                        System.out.println(this.outPutChar);
+                        counter = 1;
+
+                    }
+                    
                     Thread.sleep(500);
                 } else {
                     break;
