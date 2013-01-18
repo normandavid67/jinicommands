13ff4ea4d11e3832e5cb145791dd9b72cd2a7e19
normandavid67 <normandavid67@gmail.com>
2013-01-18 21:58:45 +0000

    Updated Help

--- JiniCommands/src/org/jini/commands/multicast/MulticastSender.java
+++ JiniCommands/src/org/jini/commands/multicast/MulticastSender.java
@@ -441,7 +441,11 @@ public class MulticastSender extends JiniCmd {
         helpTableHead.print();
         TablePrinter helpTable = new TablePrinter("Short Opt", "Long Opt", "Argument", "Desc", "Short Option Example", "Long Option Example");
         helpTable.addRow("-h", "--help", "not required", "Show this help.", "multicastsender -h", "multicastsender --help");
-        helpTable.addRow("-i", "--interval", " required", "Inetrval in Miliseconds. Default = 500 Miliseconds", "multicastsender -i 1000 ", "multicastsender --interval 1000");
+        helpTable.addRow("-i", "--interval", " required", "Interval in Miliseconds. Default = 500 Miliseconds", "multicastsender -i 1000 ", "multicastsender --interval 1000");
+        helpTable.addRow("-p", "--port", " required", "Port to which the Multicast traffic is published", "multicastsender -p 8080 ", "multicastsender --port 8080");
+        helpTable.addRow("-f", "--file", " required", "File which contains the data (Must be Readable)", "multicastsender -f /PATH/TO/FILE ", "multicastsender --file /PATH/TO/FILE");
+        helpTable.addRow("", "", " ", "'multicastsender' checks every 11th multicast if contents of file have varied.", "", "");
+        helpTable.addRow("-mcg", "--multicastgroup", " required", "Multicast Group to join", "multicastsender -mcg 224.2.2.3", "multicastsender --multicastgroup 224.2.2.3");
         helpTable.print();
 
 
